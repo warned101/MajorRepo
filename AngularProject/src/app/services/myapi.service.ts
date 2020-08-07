1da1e926a2c7aa9a3aae687787e9a987fa222311
warned101 <astitvagupta123@gmail.com>
2020-08-07 11:26:05 +0530

    First commit

---
+++ AngularProject/src/app/services/myapi.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core"
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http"
+import { Post } from "../login/post"
+
+@Injectable()
+export class MyApiService{
+
+    constructor(private httpClient:HttpClient){}
+
+    checkLoginCredentials():Observable<any>{
+        return;
+    }
+
+    checkLogs() {
+        
+    }
+
+    submitRegistrationCredentials(post:Post):Observable<any>{
+        return this.httpClient.post("https://jsonplaceholder.typicode.com/posts", post);
+    }
+
+}
